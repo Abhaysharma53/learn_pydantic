1267136d377c6bac305729fd621cc6469773d5f7
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2025-05-25 00:56:20 +0530

    data validation for business rules using field

--- pydantic_basics.py
+++ pydantic_basics.py
@@ -1,21 +1,25 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import List, Dict, Optional
 
 class Patient(BaseModel):
-    name: str
-    age: int
+    name: str = Field(max_length = 50)
+    age: int = Field(gt = 18, lt = 35)
     Gender: str
     email: EmailStr
-    allergies: Optional[List[str]] =  None
+    married: bool = Field(default= False)
+    weight: float = Field(gt = 0)
+    allergies: Optional[List[str]] =  Field(max_length= 5, default= None) #can't add more than 5 values
     contact_info: Dict[str, str]
 
 
 
-patient_info = {'name': 'Abhay Sharma', 'age': 25, 'Gender': 'Male', 'email': 'abc@gmail.com', 'contact_info': {'contact_no': '55444454343'}} 
-patient_info2 = {'name': 'Abhishek Sharma', 'age': 28, 'Gender': 'Male', 'email': 'abc@gmail.com', 'contact_info': {'contact_no': '55444454343'}} 
+#patient_info = {'name': 'Abhay Sharma', 'age': 25, 'Gender': 'Male', 'email': 'abc@gmail.com', 'contact_info': {'contact_no': '55444454343'}} 
+#patient_info2 = {'name': 'Abhishek Sharma', 'age': 28, 'Gender': 'Male', 'email': 'abc@gmail.com', 'contact_info': {'contact_no': '55444454343'}} 
+patient_info3 = {'name': 'Abhishek Sharma', 'age': 28, 'Gender': 'Male', 'email': 'abc@gmail.com', 'contact_info': {'contact_no': '55444454343'}, 'weight': 75.2, 'allergies': ['pollen', 'dust'], 'married': True} 
 
-patient_1 = Patient(**patient_info)
-patient_2 = Patient(**patient_info2)
+# patient_1 = Patient(**patient_info)
+# patient_2 = Patient(**patient_info2)
+patient_3 = Patient(**patient_info3)
 
 def get_patient_data(patient: Patient):
     print(patient.name)
@@ -23,6 +27,6 @@ def get_patient_data(patient: Patient):
     print(patient.Gender)
 
 
-get_patient_data(patient_1)
-get_patient_data(patient_2)
+#get_patient_data(patient_1)
+get_patient_data(patient_3)
 
